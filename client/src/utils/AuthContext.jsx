86fa858d1587196bd1e7fcd734acea91d82789fe
albertoviciute <albertoviciutee@gmail.com>
2024-04-16 19:54:39 +0300

    part 1

--- client/src/utils/AuthContext.jsx
+++ client/src/utils/AuthContext.jsx
@@ -1,37 +1,31 @@
-import { createContext, useState, useEffect } from 'react';
-import { jwtDecode } from 'jwt-decode';
-import { apiClient } from '../api/apis';
-
+import { createContext, useContext, useState, useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
+import { apiClient } from "../api/apis";
 
 export const AuthContext = createContext();
 
-
 export const AuthProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true);
-  const [token, setToken] = useState(localStorage.getItem('token') || null);
+  const [token, setToken] = useState(localStorage.getItem("token") || null);
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-
-    const token = localStorage.getItem('token');
-    console.log('Token from localStorage:', token);
+    const token = localStorage.getItem("token");
+    console.log("Token from localStorage:", token);
     if (token) {
-   
       const user = jwtDecode(token);
-      console.log('User from jwtDecode:', user);
-     
+      console.log("User from jwtDecode:", user);
+
       setToken(token);
       setUser(user);
     }
-   
+
     setIsLoading(false);
   }, []);
 
- 
   const loginUser = async (data) => {
     try {
-      const response = await apiClient.post('/users/login', {
-
+      const response = await apiClient.post("/users/login", {
         login: data.login,
         password: data.password,
       });
@@ -44,29 +38,40 @@ export const AuthProvider = ({ children }) => {
         // o viršuje naudojome useEffect() metodą, kad išsaugoti vartotojo duomenis, kurie yra iš tokeno, tai bus pasiekiamas visada, kai atnaujiname puslapį
         const user = jwtDecode(token);
         // Naudojame localStorage.setItem('token', token), tai todėl, kad kai atnaujiname puslapį, tai mes neprarasime prisijungimo duomenų (token ir user)
-        console.log('User from loginUser:', user);
-        localStorage.setItem('token', token);
+        console.log("User from loginUser:", user);
+        localStorage.setItem("token", token);
         setToken(token);
         setUser(user);
-       
+
         return { token, user };
       } else {
-        throw new Error('Response data is undefined');
+        throw new Error("Response data is undefined");
       }
     } catch (error) {
-      console.error('Failed to log in:', error);
+      console.error("Failed to log in:", error);
       throw error;
     }
   };
 
   const logoutUser = () => {
-    localStorage.removeItem('token');
+    localStorage.removeItem("token");
     setToken(null);
     setUser(null);
   };
 
   return (
     // siūsime value kaip props į AuthContext.Provider komponentą, kad būtu galima pasiekti duomenis kituose komponentuose globaliai
-    <AuthContext.Provider value={{ isLoading, token, user, loginUser, logoutUser }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider
+      value={{
+        isLoading,
+        isAuthenticated: !!token,
+        token,
+        user,
+        loginUser,
+        logoutUser,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
   );
 };
