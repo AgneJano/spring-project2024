d68ab26939430fcc72dcb666fa8f30462848dfe7
albertoviciute <99351981+albertoviciute@users.noreply.github.com>
2024-04-16 18:49:52 +0300

    Revert "Revert "Feat/create dashbord""

---
+++ client/src/fetching-data/UseFetch.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url);
+        setData(response.data);
+        console.log(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]); // Dependency on url so that it refetches when url changes
+
+  return { data, loading };
+};
